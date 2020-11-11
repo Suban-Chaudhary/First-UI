ae1393fbf48039e88733ae9eca703fa4eff654b6
Suban Chaudhary <chief.suban@gmail.com>
2020-11-11 10:52:56 +0545

    First UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -21,88 +21,90 @@ class _HomePageState extends State<HomePage> {
           SizedBox(width: 25),
         ],
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 25),
-              child: Row(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text("LENS", style: normalTextStyle(Colors.grey[700])),
+                    Text("Frames", style: normalTextStyle(Colors.white70)),
+                    Text("Offer", style: normalTextStyle(Colors.grey[700])),
+                    Text("Special", style: normalTextStyle(Colors.grey[700])),
+                  ],
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                "Get Awesome Glasses",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 25),
+              Container(
+                height: 150,
+                width: 350,
+                child: Image.asset(
+                  'images/glass.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.star, color: Colors.yellow, size: 30),
+                  Icon(Icons.star, color: Colors.yellow, size: 28),
+                  Icon(Icons.star, color: Colors.yellow, size: 26),
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star_half, color: Colors.yellow),
+                ],
+              ),
+              SizedBox(height: 35),
+              Container(
+                  child: Divider(color: Colors.white54, thickness: 1),
+                  padding: EdgeInsets.symmetric(horizontal: 15)),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Text(
+                          "Recommended",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 35),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Text("LENS", style: normalTextStyle(Colors.grey[700])),
-                  Text("Frames", style: normalTextStyle(Colors.white70)),
-                  Text("Offer", style: normalTextStyle(Colors.grey[700])),
-                  Text("Special", style: normalTextStyle(Colors.grey[700])),
+                  buildImageContainer(),
+                  buildImageContainer(),
+                  buildImageContainer(),
                 ],
               ),
-            ),
-            SizedBox(height: 25),
-            Text(
-              "Get Awesome Glasses",
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 25,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            SizedBox(height: 25),
-            Container(
-              height: 150,
-              width: 350,
-              child: Image.asset(
-                'images/glass.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(height: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.star, color: Colors.yellow, size: 30),
-                Icon(Icons.star, color: Colors.yellow, size: 28),
-                Icon(Icons.star, color: Colors.yellow, size: 26),
-                Icon(Icons.star, color: Colors.yellow),
-                Icon(Icons.star_half, color: Colors.yellow),
-              ],
-            ),
-            SizedBox(height: 35),
-            Container(
-                child: Divider(color: Colors.white54, thickness: 1),
-                padding: EdgeInsets.symmetric(horizontal: 15)),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Text(
-                        "Recommended",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 35),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                buildImageContainer(),
-                buildImageContainer(),
-                buildImageContainer(),
-              ],
-            ),
-            SizedBox(height: 55),
-            Text(
-              "@Copyright 2020",
-              style: normalTextStyle(Colors.grey[350]),
-            )
-          ],
+              SizedBox(height: 55),
+              Text(
+                "@Copyright 2020",
+                style: normalTextStyle(Colors.grey[350]),
+              )
+            ],
+          ),
         ),
       ),
     );
